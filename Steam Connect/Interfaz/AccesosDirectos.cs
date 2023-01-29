c3275e266911a1935ecb176efedadc688d6bc824
pepeizq <pepeizq@msn.com>
2023-01-29 09:00:27 +0100

    Agregar archivos de proyecto.

---
+++ Steam Connect/Interfaz/AccesosDirectos.cs
@@ -0,0 +1,169 @@
+﻿using Force.Crc32;
+using Herramientas;
+using Microsoft.UI.Xaml;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using ValveKeyValue;
+using Windows.Web.Http;
+using static Steam_Connect.MainWindow;
+
+namespace Interfaz
+{
+    public static class AccesosDirectos
+    {
+        public static string dominioImagenes = "https://cdn.cloudflare.steamstatic.com";
+
+        public static void Cargar()
+        {
+            ObjetosVentana.botonAccesosDirectosAñadir.Click += Añadir;
+
+            //-------------------------------------
+
+            List<SteamAccesoDirecto> datos = new List<SteamAccesoDirecto>();
+            string ficheroAccesos = GenerarRutaAccesos();
+
+            if (File.Exists(ficheroAccesos) == true) 
+            {
+                using FileStream stream = File.OpenRead(ficheroAccesos);
+                KVSerializer kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Binary);
+                datos = kv.Deserialize<List<SteamAccesoDirecto>>(stream);
+            }
+
+            ObjetosVentana.tbAccesosDirectosCargados.Text = datos.Count.ToString();
+            ObjetosVentana.botonAccesosDirectosAñadir.Tag = datos;
+        }
+
+        public static void Modificar(SteamAccesoDirecto juego, bool estado, string enlaceImagen)
+        {
+            List<SteamAccesoDirecto> datos = ObjetosVentana.botonAccesosDirectosAñadir.Tag as List<SteamAccesoDirecto>;
+
+            if (estado == true)
+            {
+                datos.Add(juego);
+            }
+            else
+            {
+                if (datos.Count > 0)
+                {
+                    int i = 0;
+                    foreach (SteamAccesoDirecto acceso in datos)
+                    {
+                        if (acceso.appname == juego.appname && acceso.exe == juego.exe)
+                        {
+                            break;
+                        }
+                     
+                        i += 1;
+                    }
+
+                    datos.RemoveAt(i);
+                }
+            }
+
+            ObjetosVentana.tbAccesosDirectosCargados.Text = datos.Count.ToString();
+        }
+
+        public static void Añadir(object sender, RoutedEventArgs e)
+        {
+            List<SteamAccesoDirecto> datos = ObjetosVentana.botonAccesosDirectosAñadir.Tag as List<SteamAccesoDirecto>;
+
+            string ficheroAccesos = GenerarRutaAccesos();
+            string carpetaImagenes = ficheroAccesos.Replace("\\shortcuts.vdf", null);
+            carpetaImagenes = carpetaImagenes + "\\grid";
+
+            foreach (SteamAccesoDirecto acceso in datos)
+            {
+                if (acceso.icon != null)
+                {
+                    if (acceso.icon.Contains("pepesteam_") == true)
+                    {
+                        string id = acceso.icon.Replace("pepesteam_", null);
+                        acceso.icon = null;
+
+                        string enlaceImagenGrid = dominioImagenes + "/steam/apps/" + id + "/library_600x900.jpg";
+                        string enlaceImagenLogo = dominioImagenes + "/steam/apps/" + id + "/logo.png";
+                        string enlaceImagenHero = dominioImagenes + "/steam/apps/" + id + "/library_hero.jpg";
+
+                        string idImagenes = GenerarIdImagen(acceso.appname, acceso.exe).ToString();
+
+                        DescargarImagen(enlaceImagenGrid, carpetaImagenes + "\\" + idImagenes + "p.png");
+                        DescargarImagen(enlaceImagenLogo, carpetaImagenes + "\\" + idImagenes + "_logo.png");
+                        DescargarImagen(enlaceImagenHero, carpetaImagenes + "\\" + idImagenes + "_hero.jpg");
+                    }
+                }               
+            }
+
+
+            using var stream = File.OpenWrite(ficheroAccesos);
+
+            var kv = KVSerializer.Create(KVSerializationFormat.KeyValues1Binary);
+            kv.Serialize(stream, datos, "shortcuts");
+        }
+
+        private static string GenerarRutaAccesos()
+        {
+            RegistryKey registro = Registry.CurrentUser.OpenSubKey("Software\\Valve\\Steam");
+            string carpetaSteam = registro.GetValue("SteamPath").ToString();
+            carpetaSteam = carpetaSteam.Replace("/", "\\");
+
+            RegistryKey registroUsuario = Registry.CurrentUser.OpenSubKey("Software\\Valve\\Steam\\ActiveProcess");
+            string usuario = registroUsuario.GetValue("ActiveUser", RegistryValueKind.DWord).ToString();
+
+            if (usuario == "0")
+            {
+                RegistryKey registroUsuario2 = Registry.CurrentUser.OpenSubKey("Software\\Valve\\Steam\\Users");
+
+                foreach (string id in registroUsuario2.GetSubKeyNames())
+                {
+                    if (id.Length > 2)
+                    {
+                        usuario = id;
+                        break;
+                    }
+                }
+            }
+
+            return carpetaSteam + "\\userdata\\" + usuario + "\\config\\shortcuts.vdf";
+        }
+
+        private static UInt64 GenerarIdImagen(string appName, string exe)
+        {
+            byte[] fusionBytes = Encoding.UTF8.GetBytes(exe + appName + "");
+            UInt64 crc = Crc32Algorithm.Compute(fusionBytes);
+            UInt64 juegoId = crc | 0x80000000;
+
+            return juegoId;
+        }
+
+        private static async void DescargarImagen(string enlace, string ubicacionynombre)
+        {
+            System.Net.Http.HttpClient cliente = new System.Net.Http.HttpClient();
+            cliente.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1");
+            byte[] ficheroBytes = await cliente.GetByteArrayAsync(enlace);
+            File.WriteAllBytes(ubicacionynombre, ficheroBytes);
+
+            //var httpResult = await httpClient.GetAsync(new Uri(enlace));
+            //using var resultStream = await httpResult.Content.stea;
+            //using var fileStream = File.Create(pathToSave);
+            //resultStream.CopyTo(fileStream);
+        }
+    }
+
+    public class SteamAccesoDirecto
+    {
+        public string appid { get; set; }
+        public string appname { get; set; }
+        public string exe { get; set; }
+        public string StartDir { get; set; }
+        public string icon { get; set; }
+        public int IsHidden { get; set; }
+        public int AllowDesktopConfig { get; set; }
+        public int AllowOverlay { get; set; }
+        public int LastPlayTime { get; set; }
+    }
+}
---
+++ Steam Connect/Plataformas/Battlenet.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.Win32;
+using static Steam_Connect.MainWindow;
+using System.Collections.Generic;
+using Herramientas;
+using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using CommunityToolkit.WinUI.UI.Controls;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media;
+using Windows.UI;
+using Interfaz;
+
+namespace Plataformas
+{
+    public static class Battlenet
+    {
+        public static async void CargarJuegosInstalados()
+        {
+            ObjetosVentana.spBattlenetJuegosInstaladosMensaje.Visibility = Visibility.Collapsed;
+            ObjetosVentana.prBattlenetJuegosInstalados.Visibility = Visibility.Visible;
+            ObjetosVentana.gvBattlenetJuegosInstalados.Visibility = Visibility.Collapsed;
+            ObjetosVentana.tbBattlenetMensajeNoJuegos.Visibility = Visibility.Collapsed;
+
+            List<BattlenetJuego> listaJuegos = new List<BattlenetJuego>();
+
+            RegistryKey registro = Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Blizzard Entertainment\\Battle.net\\Capabilities");
+            
+            if (registro != null) 
+            {
+                string rutaEjecutable = registro.GetValue("ApplicationIcon").ToString();
+
+                if (rutaEjecutable != null)
+                {
+                    rutaEjecutable = rutaEjecutable.Replace(Strings.ChrW(34).ToString(), null);
+                    rutaEjecutable = rutaEjecutable.Remove(rutaEjecutable.Length - 2, 2);
+
+                    string bbdd = await Decompiladores.CogerHtml("https://raw.githubusercontent.com/pepeizq/Database-Games/master/Base%20de%20Datos%20Plataformas/Jsons/Battlenet.json");
+
+                    if (bbdd != null)
+                    {
+                        List<BattlenetAPI> json = JsonConvert.DeserializeObject<List<BattlenetAPI>>(bbdd);
+
+                        if (json != null)
+                        {
+                            if (json.Count > 0)
+                            {
+                                foreach (BattlenetAPI juegoJson in json)
+                                {
+                                    string imagenPequeña = string.Empty;
+
+                                    if (juegoJson.imagenPequeña != string.Empty)
+                                    {
+                                        imagenPequeña = juegoJson.imagenPequeña;
+                                    }
+                                    else
+                                    {
+                                        if (juegoJson.idSteam != string.Empty)
+                                        {
+                                            imagenPequeña = AccesosDirectos.dominioImagenes + "/steam/apps/" + juegoJson.idSteam + "/header.jpg";
+                                        }
+                                    }
+
+                                    string imagenGrande = string.Empty;
+
+                                    if (juegoJson.imagenGrande != string.Empty)
+                                    {
+                                        imagenGrande = juegoJson.imagenGrande;
+                                    }
+                                    else
+                                    {
+                                        if (juegoJson.idSteam != string.Empty)
+                                        {
+                                            imagenGrande = AccesosDirectos.dominioImagenes + "/steam/apps/" + juegoJson.idSteam + "/library_600x900.jpg";
+                                        }
+                                    }
+
+                                    listaJuegos.Add(new BattlenetJuego(juegoJson.nombre, 
+                                                        Strings.ChrW(34).ToString() + rutaEjecutable + Strings.ChrW(34).ToString() + " --exec=" + Strings.ChrW(34).ToString() + "launch " + juegoJson.idBattlenet + Strings.Chr(34).ToString(),
+                                                        imagenPequeña, imagenGrande, juegoJson.idSteam));
+                                }
+                            }
+                        }
+                    }
+
+                    if (listaJuegos.Count > 0)
+                    {
+                        ObjetosVentana.spBattlenetJuegosInstaladosMensaje.Visibility = Visibility.Visible;
+                        ObjetosVentana.tbBattlenetMensajeNoJuegos.Visibility = Visibility.Collapsed;
+                        ObjetosVentana.gvBattlenetJuegosInstalados.Visibility = Visibility.Visible;
+                        ObjetosVentana.gvBattlenetJuegosInstalados.Items.Clear();
+
+                        listaJuegos.Sort(delegate (BattlenetJuego c1, BattlenetJuego c2) { return c1.nombre.CompareTo(c2.nombre); });
+
+                        foreach (BattlenetJuego juego in listaJuegos)
+                        {
+                            StackPanel sp = new StackPanel
+                            {
+                                Orientation = Orientation.Vertical
+                            };
+
+                            ToggleSwitch ts = new ToggleSwitch
+                            {
+                                RequestedTheme = ElementTheme.Dark,
+                                OnContent = string.Empty,
+                                OffContent = string.Empty,
+                                Margin = new Thickness(10, 0, 0, 0)
+                            };
+
+                            sp.Children.Add(ts);
+
+                            ImageEx imagen = new ImageEx
+                            {
+                                IsCacheEnabled = true,
+                                EnableLazyLoading = true,
+                                Stretch = Stretch.UniformToFill,
+                                Source = juego.imagenGrande,
+                                CornerRadius = new CornerRadius(2)
+                            };
+
+                            sp.Children.Add(imagen);
+
+                            Button2 botonJuego = new Button2
+                            {
+                                Content = sp,
+                                Margin = new Thickness(0),
+                                Padding = new Thickness(0),
+                                Background = new SolidColorBrush((Color)Application.Current.Resources["ColorPrimario"]),
+                                BorderBrush = new SolidColorBrush((Color)Application.Current.Resources["ColorPrimario"]),
+                                BorderThickness = new Thickness(3),
+                                Tag = juego,
+                                MaxWidth = 300,
+                                CornerRadius = new CornerRadius(5)
+                            };
+
+                            botonJuego.Click += JuegoClick;
+                            botonJuego.PointerEntered += Animaciones.EntraRatonBoton2;
+                            botonJuego.PointerExited += Animaciones.SaleRatonBoton2;
+
+                            GridViewItem item = new GridViewItem
+                            {
+                                Content = botonJuego,
+                                Margin = new Thickness(5, 0, 5, 10)
+                            };
+
+                            ObjetosVentana.gvBattlenetJuegosInstalados.Items.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        ObjetosVentana.tbBattlenetMensajeNoJuegos.Visibility = Visibility.Visible;
+                    }
+                }
+                else
+                {
+                    ObjetosVentana.tbBattlenetMensajeNoJuegos.Visibility = Visibility.Visible;
+                }
+            }
+            else
+            {
+                ObjetosVentana.tbBattlenetMensajeNoJuegos.Visibility = Visibility.Visible;
+            }
+
+            ObjetosVentana.prBattlenetJuegosInstalados.Visibility = Visibility.Collapsed;
+        }
+
+        private static void JuegoClick(object sender, RoutedEventArgs e)
+        {
+            Button boton = sender as Button;
+            BattlenetJuego juego = boton.Tag as BattlenetJuego;
+
+            StackPanel sp = boton.Content as StackPanel;
+            ToggleSwitch ts = sp.Children[0] as ToggleSwitch;
+
+            if (ts.IsOn == true)
+            {
+                ts.IsOn = false;
+            }
+            else
+            {
+                ts.IsOn = true;
+            }
+
+            SteamAccesoDirecto acceso = new SteamAccesoDirecto
+            {
+                appname = juego.nombre,
+                exe = juego.ejecutable
+            };
+
+            if (juego.idSteam != null)
+            {
+                acceso.icon = "pepesteam_" + juego.idSteam;
+            }
+
+            AccesosDirectos.Modificar(acceso, ts.IsOn, juego.imagenGrande);
+        }
+    }
+
+    public class BattlenetJuego
+    {
+        public string nombre { get; set; }
+        public string ejecutable { get; set; }
+        public string imagenPequeña { get; set; }
+        public string imagenGrande { get; set; }
+        public string idSteam { get; set; }
+
+        public BattlenetJuego(string Nombre, string Ejecutable, string ImagenPequeña, string ImagenGrande, string IdSteam)
+        {
+            nombre = Nombre;
+            ejecutable = Ejecutable;
+            imagenPequeña = ImagenPequeña;
+            imagenGrande = ImagenGrande;
+            idSteam = IdSteam; 
+        }
+    }
+}
